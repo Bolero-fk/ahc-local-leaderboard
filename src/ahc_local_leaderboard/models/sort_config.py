06ac1c8627cbe951df060c43a7a96e45e802c62b
Bolero-fk <fuku_0000@outlook.jp>
2024-12-11 22:19:27 +0900

    add DetailScoreRecordsSortConfig

--- src/ahc_local_leaderboard/models/sort_config.py
+++ src/ahc_local_leaderboard/models/sort_config.py
@@ -1,3 +1,8 @@
+from ahc_local_leaderboard.utils.relative_score_calculater import (
+    RelativeScoreCalculaterInterface,
+)
+
+
 class SortConfig:
     """Viewer用の表示順序設定クラス"""
 
@@ -25,3 +30,18 @@ class SummaryScoreRecordsSortConfig(SortConfig):
             self.key = lambda record: record.total_absolute_score
         elif column == "rel":
             self.key = lambda record: record.total_relative_score
+
+
+class DetailScoreRecordsSortConfig(SortConfig):
+    """DetailScoreRecords用の表示順序設定クラス"""
+
+    def __init__(self, column: str, order: str, relative_calculator: RelativeScoreCalculaterInterface):
+        assert column in ["id", "abs", "rel"]
+        super().__init__(column, order)
+
+        if column == "id":
+            self.key = lambda record: record.file_name
+        elif column == "abs":
+            self.key = lambda record: record.absolute_score if record.absolute_score is not None else int("inf")
+        elif column == "rel":
+            self.key = lambda record: record.calculate_relative_score(relative_calculator)
